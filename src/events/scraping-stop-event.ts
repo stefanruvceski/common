a81e255846d695c5298c99baa020370cf84ad499
stefan.ruvceski <stefanruvceski@gmail.com>
2022-07-09 19:09:52 +0200

    Updates

---
+++ src/events/scraping-stop-event.ts
@@ -0,0 +1,8 @@
+import { Subjects } from "./subjects";
+
+export interface ScrapingStopEvent {
+  subject: Subjects.ScrapingStop;
+  data: {
+    jobId: string | number;
+  };
+}
--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -2,6 +2,7 @@ export enum Subjects {
   ScraperCreated = "scraper:created",
   ScraperUpdated = "scraper:updated",
   ScrapingStart = "scraping:start",
+  ScrapingStop = "scraping:stop",
   ScrapingSchedule = "scraping:schedule",
   ScrapingFinished = "scraping:finished",
 }
